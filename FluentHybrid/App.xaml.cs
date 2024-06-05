f616916a0c2c57562fd43a6b2cf0067e17ce30f8
Dave Parkes <david.parkes2@outlook.com>
2024-06-05 11:32:34 +0100

    Add project files.

---
+++ FluentHybrid/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FluentHybrid;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ FluentHybrid/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentHybrid;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
